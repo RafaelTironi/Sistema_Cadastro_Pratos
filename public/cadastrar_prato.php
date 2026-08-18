b8e9c470c5dd15f679c7ab2cc7dc30c19b7e09ac
Juana.2213 <Juana_franciscao@estudante.sesisenai.org.br>
2026-08-18 08:25:59 -0300

    add- fiz o codigo do cadastrar_prato

---
+++ public/cadastrar_prato.php
@@ -0,0 +1,28 @@
+<?php
+
+
+include "../infra/conexao.php";
+
+$nome = $_POST["nome"];
+$descricao = $_POST["descricao"];
+$preco = $_POST["preco"];
+$categoria = $_POST["categoria"]
+//query
+$sql = "INSERT INTO livros (nome, descricao ,preco, categoria) VALUES (?,?,?,?)";
+
+if(stmt){
+
+$stmt=mysqli_prepare($conexao, $sql)
+
+mysqli_stmt_bind_param($stmt, "ssds", $nome, $descricao, $preco, $categoria );
+
+mysqli_stmt_execute($stmt);
+
+mysqli_stmt_close($stmt);
+}
+
+
+header("Location: ../index.php");
+exist();
+
+?>
