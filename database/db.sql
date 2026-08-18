e00d6208e713ca3a85ad2c5daad6c41e67275171
Rafael_Icaro <rafael_tironi1@estudante.sesisenai.org.br>
2026-08-18 10:59:04 -0300

    up: arrumei o bd e arrumei o excluir

--- database/db.sql
+++ database/db.sql
@@ -1,20 +1,19 @@
-CREATE DATABASE sistema_pratos_juErafa;
+CREATE DATABASE IF NOT EXISTS sistema_pratos_juErafa;
+
 USE sistema_pratos_juErafa;
 
-
-Create TABLE usuarios (
+CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(200) NOT NULL,
     email VARCHAR(200) NOT NULL UNIQUE
 );
 
-CREATE TABLE pratos (
+CREATE TABLE IF NOT EXISTS pratos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(200) NOT NULL,
-    descricao TEXT NOT NULL,
+    descricao TEXT,
     preco DECIMAL(10, 2) NOT NULL,
-    categoria VARCHAR(100) NOT NULL,
+    categoria VARCHAR(100),
     usuario_id INT NOT NULL,
-
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
--- public/excluir.php
+++ public/excluir.php
@@ -1,12 +1,12 @@
 <?php 
 
-include"../infra/conexao.php"
+include"../infra/conexao.php";
 
 $id=$_GET["id"];
 
 $sql = "DELETE FROM pratos WHERE id = ?";
 
-$stmt=mysqli_prepare($conexao, $sql)
+$stmt=mysqli_prepare($conexao, $sql);
 
 mysqli_stmt_bind_param($stmt, "i", $id);
 
@@ -14,7 +14,7 @@ mysqli_stmt_execute($stmt);
 
 mysqli_stmt_close($stmt);
 
-header("Location:. ./index.php");
+header("Location:../index.php");
 exit;
 
 ?>
