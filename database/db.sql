69db15fd8eba99f51eabb3f7f4968742e271ef93
Rafael_Icaro <rafael_tironi1@estudante.sesisenai.org.br>
2026-08-18 08:55:44 -0300

    add: criei a tabela de usuarios ligada a tabela de pratos.

--- database/db.sql
+++ database/db.sql
@@ -1,10 +1,20 @@
 CREATE DATABASE sistema_pratos_juErafa;
 USE sistema_pratos_juErafa;
 
+
+Create TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL UNIQUE,
+);
+
 CREATE TABLE pratos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(200) NOT NULL,
     descricao TEXT,
     preco DECIMAL(10, 2) NOT NULL,
-    categoria VARCHAR(100)
+    categoria VARCHAR(100),
+    usuario_id INT NOT NULL,
+
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
