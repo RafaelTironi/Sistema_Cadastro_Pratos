34fae85d8859895a69dca53ed42385973db7b6b9
Juana.2213 <Juana_franciscao@estudante.sesisenai.org.br>
2026-08-18 09:15:46 -0300

    add - fiz o codigo cadastrar usuario

---
+++ public/cadastrar_usu.php
@@ -0,0 +1,26 @@
+<?php
+
+
+include "../infra/conexao.php";
+
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+
+$sql = "INSERT INTO usuarios (nome, email) VALUES (?,?)";
+
+
+$stmt=mysqli_prepare($conexao, $sql);
+
+if(stmt){
+
+mysqli_stmt_bind_param($stmt, "ss", $nome, $email);
+
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
+
+}
+
+header("Location: ../index.php");
+exist();
+
+?>
